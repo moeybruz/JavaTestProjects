16344902d5e1731c5f994b05d0821be18e9e765b
moeybruz <54069766+moeybruz@users.noreply.github.com>
2019-12-24 13:56:31 +1100

    Create Inheritent.java

---
+++ InheritentFmly/src/Inheritent.java
@@ -0,0 +1,9 @@
+public class Inheritent {
+    String name;
+    int age;
+    String profession;
+
+    void intro(){
+        System.out.println("My name is " + name + ", I am " + age + " years old and I am a " + profession + ".");
+    }
+}
