8d00b4e5f41696d757cf9be7a49021cbc9acf5f5
moeybruz <54069766+moeybruz@users.noreply.github.com>
2019-12-24 13:40:33 +1100

    Create main.java

---
+++ AreaCalculator/src/main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(8.5));
+        System.out.println(AreaCalculator.area(-1.0, 4.0));
+    }
+}
