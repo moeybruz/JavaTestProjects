8e0ae0498225016a7c4cfa31168787d1f42a4003
moeybruz <54069766+moeybruz@users.noreply.github.com>
2019-12-24 13:57:33 +1100

    Create PeopleData.java

---
+++ InheritentFmly/src/PeopleData.java
@@ -0,0 +1,23 @@
+public class PeopleData {
+    public static void main(String[] args) {
+        Inheritent boy1 = new Inheritent();
+        boy1.name = "David Abouhaider";
+        boy1.age = 25;
+        boy1.profession = "musician";
+        boy1.intro();
+
+        Inheritent girl1 = new Inheritent();
+        girl1.name = "Stella Zhang";
+        girl1.age = 30;
+        girl1.profession = "business analyst";
+        girl1.intro();
+
+        Inheritent boy2 = new Inheritent();
+        boy2.name = "Brett Pitt";
+        boy2.age = 55;
+        boy2.profession = "actor";
+        boy2.intro();
+
+
+    }
+}
