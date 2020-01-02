1e8d58a44d9100b738ea2b56b8cf3d7b61550f3f
moeybruz <54069766+moeybruz@users.noreply.github.com>
2020-01-02 12:34:17 +1100

    Create Main.java

---
+++ MetricConversion/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+//        FeetInch2CM.calcFeelAndInchesToCentimeters(10,11);
+        FeetInch2CM.calcFeelAndInchesToCentimeters(156);
+    }
+}
