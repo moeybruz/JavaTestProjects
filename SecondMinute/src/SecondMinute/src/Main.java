676d8826329e473def13f5afbb1c871b91219f0e
moeybruz <54069766+moeybruz@users.noreply.github.com>
2020-01-02 12:40:19 +1100

    Create Main.java

---
+++ SecondMinute/src/SecondMinute/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SecondMinute.getDurationString(65, 45));
+        System.out.println(SecondMinute.getDurationString(3945));
+    }
+}
