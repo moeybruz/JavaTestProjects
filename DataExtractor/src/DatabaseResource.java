268ce8ae06bc3d0a5f9537100e04d7b34065c0c1
moeybruz <54069766+moeybruz@users.noreply.github.com>
2020-04-01 13:36:23 +1100

    Create DatabaseResource.java

---
+++ DataExtractor/src/DatabaseResource.java
@@ -0,0 +1,41 @@
+package model;
+
+public class DatabaseResource {
+
+    private String username;
+    private String password;
+    private String url;
+    private String query;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
